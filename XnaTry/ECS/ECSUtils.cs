03fe5dae14ccd55f54d47e8939d3994d5a3b80d7
Giora Guttsait <giora111@gmail.com>
2016-08-03 23:42:12 +0300

    Added EcsUtil class for utility and extention methods

---
+++ XnaTry/ECS/ECSUtils.cs
@@ -0,0 +1,18 @@
+﻿using ECS.Interfaces;
+using System;
+
+namespace ECS
+{
+    public static class EcsUtils
+    {
+        static public IComponentContainer AddToPool(this IEntityPool pool, IEntity entity)
+        {
+            if (pool == null)
+                throw new ArgumentNullException("pool");
+            if (entity == null)
+                throw new ArgumentNullException("entity");
+            pool.Add(entity);
+            return pool.GetComponents(entity);
+        }
+    }
+}
