67e55be8c694f6b1bf299f8d9b9ea621a067c91a
Giora Guttsait <giora111@gmail.com>
2016-08-03 23:36:40 +0300

    Added Equality Members for Entity class

--- XnaTry/ECS/BaseTypes/Entity.cs
+++ XnaTry/ECS/BaseTypes/Entity.cs
@@ -5,6 +5,29 @@ namespace ECS.BaseTypes
 {
     public class Entity : IEntity
     {
+        #region Equality Members
+
+        protected bool Equals(Entity other)
+        {
+            return Id.Equals(other.Id);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+            return obj.GetType() == GetType() && Equals((Entity)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        #endregion
+
         public Guid Id { get; set; }
 
         public Entity(Guid id)
