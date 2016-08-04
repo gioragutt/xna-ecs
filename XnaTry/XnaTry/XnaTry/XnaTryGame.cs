2f0a0e1eb68498b42f301d1cb0550c48c34d9364
Giora Guttsait <giora111@gmail.com>
2016-08-04 22:57:30 +0300

    Removed redundant Initialize method override in XnaTryGame

--- XnaTry/XnaTry/XnaTry/XnaTryGame.cs
+++ XnaTry/XnaTry/XnaTry/XnaTryGame.cs
@@ -28,19 +28,6 @@ namespace XnaTry
         }
 
         /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
-        protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
-
-            base.Initialize();
-        }
-
-        /// <summary>
         /// LoadContent will be called once per game and is the place to load
         /// all of your content.
         /// </summary>
