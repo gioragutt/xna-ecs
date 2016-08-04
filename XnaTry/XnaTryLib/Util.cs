b71335710348909fc9df9ad3a1e189c49777cd2f
Giora Guttsait <giora111@gmail.com>
2016-08-04 22:43:43 +0300

    Added some util methods to Util class
    * added argument null assertion methods
    * made GetEnumValues public, and refactored it to use Linq syntax

--- XnaTry/XnaTryLib/Util.cs
+++ XnaTry/XnaTryLib/Util.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace XnaTryLib
 {
     internal class Util
     {
-        static IEnumerable<T> GetEnumValues<T>()
+        public static IEnumerable<T> GetEnumValues<T>()
         {
-            Array values = Enum.GetValues(typeof(T));
-            foreach (var value in values)
-                yield return (T)value;
+            return from object value in Enum.GetValues(typeof(T)) select (T)value;
+        }
+
+        public static void AssertArgumentNotNull(object argument, string parameterName)
+        {
+            if (ReferenceEquals(argument, null))
+                throw new ArgumentNullException(parameterName);
+        }
+
+        public static void AssertStringArgumentNotNull(string argument, string parameterName)
+        {
+            if (string.IsNullOrEmpty(argument))
+                throw new ArgumentNullException(parameterName);
         }
     }
 }
