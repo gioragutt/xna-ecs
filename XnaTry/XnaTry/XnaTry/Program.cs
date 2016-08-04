dcf8ace84eca09253e2019cfe28dabf557ce5e52
Giora Guttsait <giora111@gmail.com>
2016-08-04 22:41:28 +0300

    Cleaned project junk

--- XnaTry/XnaTry/XnaTry/Program.cs
+++ XnaTry/XnaTry/XnaTry/Program.cs
@@ -6,20 +6,12 @@ namespace XnaTry
 #if WINDOWS || XBOX
     static class Program
     {
-        static void TestECS()
-        {
-            var manager = new EntityManager();
-            var entity1 = manager.CreateEntity();
-            Debug.WriteLine("Entity GUID is " + entity1.Id);
-        }
-
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         static void Main(string[] args)
         {
-            TestECS();
-            using (XnaTryGame game = new XnaTryGame())
+            using (var game = new XnaTryGame())
             {
                 game.Run();
             }
