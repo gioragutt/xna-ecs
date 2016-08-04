0d583b465590344e89c93aa3589ea9aae7024104
Giora Guttsait <giora111@gmail.com>
2016-08-04 22:39:04 +0300

    Change SystemManager to save IEntityPool, made methods public

--- XnaTry/ECS/Managers/SystemManager.cs
+++ XnaTry/ECS/Managers/SystemManager.cs
@@ -7,22 +7,21 @@ namespace ECS.Managers
     public class SystemManager
     {
         public ISystemContainer Systems { get; }
-        public EntityPool Entities { get; }
+        public IEntityPool Entities { get; }
 
-        SystemManager(EntityPool entities)
+        public SystemManager(IEntityPool entities)
         {
             Systems = new SystemContainer();
             Entities = entities;
         }
 
-        void AddSystem<TComponent>(ISystem<TComponent> system) where TComponent : class, IComponent
+        public void AddSystem<TComponent>(ISystem<TComponent> system) where TComponent : class, IComponent
         {
             Systems.Add(system);
         }
 
-        void Update(long delta)
+        public void Update(long delta)
         {
-            foreach (var system in Systems.GetAll().Where(s => s.Enabled))
             foreach (var system in Systems.All.Where(s => s.Enabled))
             {
                 system.Update(Entities, delta);
