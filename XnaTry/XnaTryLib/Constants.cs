9bab3ca41b1cce3e201c4ce62af20eeda768e2e3
Giora Guttsait <giora111@gmail.com>
2016-08-05 20:18:32 +0300

    Added a Keyboard input component
    * saves key settings to allow custom keyboard arrangements

--- XnaTry/XnaTryLib/Constants.cs
+++ XnaTry/XnaTryLib/Constants.cs
@@ -4,8 +4,12 @@
     {
         public const float DefaultScale = 1f;
         public const float DefaultRotation = 0f;
+
         public const int DebugPrintInitialX = 10;
         public const int DebugPrintInitialY = 10;
         public const int DebugPrintSpacing = 0;
+
+        public const float FullPositiveInput = 1;
+        public const float FullNegativeInput = -1;
     }
 }
---
+++ XnaTry/XnaTryLib/ECS/Components/KeyboardDirectionalInput.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework.Input;
+
+namespace XnaTryLib.ECS.Components
+{
+    public struct KeyboardSettings
+    {
+        public Keys Left { get; set; }
+        public Keys Right { get; set; }
+        public Keys Up { get; set; }
+        public Keys Down { get; set; }
+
+        public KeyboardSettings(Keys left = Keys.Left, Keys right = Keys.Right, Keys up = Keys.Up, Keys down = Keys.Down)
+        {
+            Left = left;
+            Right = right;
+            Up = up;
+            Down = down;
+        }
+    }
+
+    public class KeyboardDirectionalInput : DirectionalInput
+    {
+        private KeyboardSettings Settings { get; }
+
+        private static readonly KeyboardSettings DefaultSettings = new KeyboardSettings
+        {
+            Down = Keys.Down,
+            Up = Keys.Up,
+            Right = Keys.Right,
+            Left = Keys.Left
+        };
+
+        /// <summary>
+        /// Initializes the input component with the key settings
+        /// </summary>
+        /// <param name="settings">Settings; default (arrow keys) if null specified</param>
+        public KeyboardDirectionalInput(KeyboardSettings? settings = null)
+        {
+            Settings = settings ?? DefaultSettings;
+        }
+
+        public override void Update(long delta)
+        {
+            Vertical = Horizontal = 0;
+            var keyboard = Keyboard.GetState();
+            if (keyboard.IsKeyDown(Settings.Down))
+                Vertical += Constants.FullPositiveInput;
+            if (keyboard.IsKeyDown(Settings.Up))
+                Vertical += Constants.FullNegativeInput;
+            if (keyboard.IsKeyDown(Settings.Right))
+                Horizontal += Constants.FullPositiveInput;
+            if (keyboard.IsKeyDown(Settings.Left))
+                Horizontal += Constants.FullNegativeInput;
+        }
+    }
+}
