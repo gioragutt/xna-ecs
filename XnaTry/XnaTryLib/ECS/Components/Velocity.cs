9fc4fa2f12cbf55373b6076e847ee96cc422a560
Giora Guttsait <giora111@gmail.com>
2016-08-05 21:52:08 +0300

    Added Velocity component

---
+++ XnaTry/XnaTryLib/ECS/Components/Velocity.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace XnaTryLib.ECS.Components
+{
+    public class Velocity : BaseComponent
+    {
+        public Vector2 VelocityVector { get; set; }
+        public float X => VelocityVector.X;
+        public float Y => VelocityVector.Y;
+    }
+}
