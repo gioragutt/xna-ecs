edf33291d42337b57349c594f5ab6854a4ca4e1c
Giora Guttsait <giora111@gmail.com>
2016-08-05 20:17:32 +0300

    Added a base abstract class for receiving input
    * the component exports a horizontal and vertical input value, ranging from 1 to -1

---
+++ XnaTry/XnaTryLib/ECS/Components/DirectionalInput.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace XnaTryLib.ECS.Components
+{
+    /// <summary>
+    /// Base class for deriving directional movement input from an outer source
+    /// </summary>
+    public abstract class DirectionalInput : BaseComponent
+    {
+        private static float ClampInput(float input)
+        {
+            return MathHelper.Clamp(input, Constants.FullNegativeInput, Constants.FullPositiveInput);
+        }
+
+        private float horizontal;
+        private float vertical;
+
+        /// <summary>
+        /// Indicates the input from the horizontal direction
+        /// </summary>
+        /// <remarks>
+        /// Values between Constants.FullPositiveInput to Constants.FullNegativeInput.
+        /// </remarks>
+        public float Horizontal
+        {
+            get
+            {
+                return horizontal;
+            }
+            protected set
+            {
+                horizontal = ClampInput(value);
+            }
+        }
+
+        /// <summary>
+        /// Indicates the input from the veritcal direction
+        /// </summary>
+        /// <remarks>
+        /// Values between Constants.FullPositiveInput to Constants.FullNegativeInput.
+        /// </remarks>
+        public float Vertical
+        {
+            get
+            {
+                return vertical;
+            }
+            protected set
+            {
+                vertical = ClampInput(value);
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Horizontal: ( {0} ) Vertical: ( {1} )", Horizontal.ToString("0.00"), Vertical.ToString("0.00"));
+        }
+
+        public abstract void Update(long delta);
+    }
+}
