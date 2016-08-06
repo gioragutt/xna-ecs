6e383f6cb8b204d712589dee46b9a45e172542f9
Giora Guttsait <giora111@gmail.com>
2016-08-06 18:35:50 +0300

    Added RotateToMouse component
    * probably just a debug component, for now. Might want to separate it to a different system

---
+++ XnaTry/XnaTryLib/ECS/Components/RotateToMouse.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace XnaTryLib.ECS.Components
+{
+    public class RotateToMouse : BaseComponent
+    {
+        public Vector2 MousePosition => Mouse.GetState().GetPosition();
+    }
+}
