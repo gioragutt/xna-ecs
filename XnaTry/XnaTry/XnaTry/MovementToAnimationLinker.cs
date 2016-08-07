fde72ad07c566904d99a0d10dbcb803dccfd8551
Giora Guttsait <giora111@gmail.com>
2016-08-07 21:56:27 +0300

    Added a Linker that links between entity input and animation

---
+++ XnaTry/XnaTry/XnaTry/MovementToAnimationLinker.cs
@@ -0,0 +1,40 @@
+﻿using XnaTryLib;
+using XnaTryLib.ECS.Components;
+
+namespace XnaTry
+{
+    public class MovementToAnimationLinker : Linker<DirectionalInput, StateBasedAnimation<MovementDirection>>
+    {
+        public override void Link()
+        {
+            if (!First.IsMoving())
+            {
+                if (!Second.Enabled)
+                    return;
+
+                Second.Enabled = false;
+                return;
+            }
+
+            Second.Enabled = true;
+            var direction = Second.DefaultState;
+
+            if (First.Vertical > 0)
+                direction = MovementDirection.Down;
+            else if (First.Vertical < 0)
+                direction = MovementDirection.Up;
+            if (First.Horizontal > 0)
+                direction = MovementDirection.Right;
+            else if (First.Horizontal < 0)
+                direction = MovementDirection.Left;
+
+            Second.CurrentState = direction;
+        }
+
+        public MovementToAnimationLinker(DirectionalInput first, StateBasedAnimation<MovementDirection> second) 
+            : base(first, second)
+        {
+
+        }
+    }
+}
