2fcf0da5a7de745598ea9ca169c12ba45e043b9b
Giora Guttsait <giora111@gmail.com>
2016-08-07 21:45:56 +0300

    Added a Linker component and system to help link between components (probably logic -> ui)
    Saves two components (although can make more implementations, like tuple) and uses their Link method to update their states

---
+++ XnaTry/XnaTryLib/ECS/Components/Linker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XnaTryLib.ECS.Components
+{
+    public abstract class Linker : BaseComponent
+    {
+        public abstract void Link();
+
+        protected Linker(bool enabled = true) : base(enabled)
+        {
+        }
+    }
+
+    public abstract class Linker<T1, T2> : Linker
+    {
+        protected Linker(T1 first, T2 second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        protected T1 First { get; set; }
+        protected T2 Second { get; set; }
+
+        public override abstract void Link();
+    }
+}
---
+++ XnaTry/XnaTryLib/ECS/Systems/LinkerSystem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ECS.Interfaces;
+using XnaTryLib.ECS.Components;
+
+namespace XnaTryLib.ECS.Systems
+{
+    public class LinkerSystem : BaseSystem
+    {
+        public override void Update(ICollection<IComponentContainer> entities, long delta)
+        {
+            entities.Select(c => c.Get<Linker>()).ToList().ForEach(linker => linker.Link());
+        }
+
+        public override ICollection<IComponentContainer> GetRelevant(IEntityPool pool)
+        {
+            return pool.AllThat(c => c.Has<Linker>()).ToList();
+        }
+    }
+}
