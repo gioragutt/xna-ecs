b3f42c213a5cc545926d989770e61b3ec09f5839
Giora Guttsait <giora111@gmail.com>
2016-08-07 21:41:18 +0300

    Added a system for controlling animations
    * Load content is probably a have operation (although it doesn't always load the content)
    * Consider talking to others about way to improve loading the Content

---
+++ XnaTry/XnaTryLib/ECS/Systems/AnimationSystem.cs
@@ -0,0 +1,33 @@
+﻿using ECS.Interfaces;
+using Microsoft.Xna.Framework.Content;
+using System.Collections.Generic;
+using System.Linq;
+using XnaTryLib.ECS.Components;
+
+namespace XnaTryLib.ECS.Systems
+{
+    public class AnimationSystem : BaseSystem
+    {
+        public ContentManager Content { get; set; }
+
+        public override void Update(ICollection<IComponentContainer> entities, long delta)
+        {
+            foreach (var entity in entities)
+            {
+                HandleAnimation(entity, delta);
+            }
+        }
+
+        private void HandleAnimation(IComponentContainer entity, long delta)
+        {
+            var animation = entity.Get<Animation>();
+            animation.LoadContent(Content);
+            animation.Update(delta);
+        }
+
+        public override ICollection<IComponentContainer> GetRelevant(IEntityPool pool)
+        {
+            return pool.AllThat(c => c.Has<Animation>() && c.Has<Sprite>()).ToList();
+        }
+    }
+}
