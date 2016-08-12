f9176c0ee22bb2d19985d7e71a5c5ee48cdfd319
Giora Guttsait <giora111@gmail.com>
2016-08-12 22:21:45 +0300

    Added TeamData class, which holds relevant data about each team

---
+++ XnaTry/XnaTryLib/TeamData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace XnaTry
+{
+    public struct TeamData : IEquatable<TeamData>
+    {
+        public string Name { get; set; }
+        public Color Color { get; set; }
+        public string TeamFrameTextureAsset { get; set; }
+
+        public bool Equals(TeamData other) { return Name.Equals(other.Name); }
+    }
+}
