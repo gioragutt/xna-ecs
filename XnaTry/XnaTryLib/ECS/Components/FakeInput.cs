005ec84983ab41e143cf624bf600f4c365d9805f
Giora Guttsait <giora111@gmail.com>
2016-08-12 22:22:14 +0300

    Added FakeInput, which randomizes input values at constant intervals

---
+++ XnaTry/XnaTryLib/ECS/Components/FakeInput.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using XnaTryLib.ECS.Components;
+
+namespace XnaTry
+{
+    public class FakeInput : DirectionalInput
+    {
+        Random Random { get; } = new Random();
+        private long timer = 0;
+        private const long TimePerUpdate = 150;
+
+        public override void Update(long delta)
+        {
+            if (!ShouldUpdate(delta))
+                return;
+
+            Horizontal = GetRandomInputValue();
+            Vertical = GetRandomInputValue();
+        }
+
+        private bool ShouldUpdate(long delta)
+        {
+            timer += delta;
+            if (timer < TimePerUpdate)
+                return false;
+            timer -= TimePerUpdate;
+            return true;
+        }
+
+        private float GetRandomInputValue() { return (float)Random.NextDouble() * Random.Next(-1, 2); }
+    }
+}
