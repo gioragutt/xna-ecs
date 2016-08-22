16219d820da8a54c8105944f85963129d5a7072a
Giora Guttsait <giora111@gmail.com>
2016-08-22 23:26:39 +0300

    Added a debug EmsClient to ConsoleServer that informs of all messages

--- XnaTry/ConsoleServer/Program.cs
+++ XnaTry/ConsoleServer/Program.cs
@@ -1,11 +1,33 @@
-﻿using XnaServerLib;
+﻿using System;
+using System.Text;
+using EMS;
+using XnaCommonLib;
+using XnaServerLib;
 
 namespace ConsoleServer
 {
+    public class ServerMessagesPrinter : EmsClient, IDisposable
+    {
+        public ServerMessagesPrinter()
+        {
+            SubscribeToAll(Callback_ToAll);
+        }
+
+        private static void Callback_ToAll(EventMessageData eventMessageData)
+        {
+            Console.WriteLine("Received message: {0}", eventMessageData.Name);
+        }
+
+        public void Dispose()
+        {
+            UnsubscribeFromAll();
+        }
+    }
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            var printer = new ServerMessagesPrinter();
             var server = new Server();
             server.Listen();
         }
