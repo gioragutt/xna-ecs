2a4fc9c615579e88a2543278b1abf30d5c8d73ec
Giora Guttsait <giora111@gmail.com>
2016-08-23 13:44:40 +0300

    Added the SpriteEffect Component
    * SpriteEffect is used to apply shader effects to your sprites

---
+++ XnaTry/XnaClientLib/ECS/Compnents/SpriteEffect.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using XnaCommonLib.ECS.Components;
+
+namespace XnaClientLib.ECS.Compnents
+{
+    public class SpriteEffect : Component, IContentRequester
+    {
+        public Effect Effect { get; private set; }
+        public string EffectAsset { get; }
+        public string AppliedPass { get; private set; }
+
+        public SpriteEffect(string asset) { EffectAsset = asset; }
+
+        public void LoadContent(ContentManager content)
+        {
+            Effect = content.Load<Effect>(EffectAsset);
+        }
+
+        public void ApplyPass(string passName)
+        {
+            AppliedPass = passName;
+            Enabled = true;
+        }
+
+        public void ResetPass()
+        {
+            AppliedPass = string.Empty;
+            Enabled = false;
+        }
+    }
+}
