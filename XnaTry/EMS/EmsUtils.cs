6bbb1e330558b32e136dcc5782f01bea56052878
Giora Guttsait <giora111@gmail.com>
2016-08-24 15:21:25 +0300

    Added an extension method to read GUID fields from a JObject

--- XnaTry/EMS/EmsUtils.cs
+++ XnaTry/EMS/EmsUtils.cs
@@ -25,12 +25,29 @@ namespace EMS
         /// </summary>
         /// <param name="jObject">A JObject containing an event message</param>
         /// <returns>true if the transmitted value was found and was true; otherwise false</returns>
-        /// /// <exception cref="System.ArgumentNullException">if Jobject is null</exception>
+        /// <exception cref="System.ArgumentNullException">if jObject is null</exception>
         public static bool GetTransmitted(this JObject jObject)
         {
             AssertArgumentNotNull(jObject, "jObject");
 
             return jObject.GetValue(Constants.TransmittedField)?.Value<bool?>() ?? false;
+        }
+
+        /// <summary>
+        /// Gets the value of a GUID field
+        /// </summary>
+        /// <param name="jObject">A JObject containing the guid</param>
+        /// <param name="propName">The name of the guid property</param>
+        /// <returns>A read GUID if exists; otherwise Guid.Empty</returns>
+        /// <exception cref="System.ArgumentNullException">if jObject is null</exception>
+        /// <exception cref="System.ArgumentNullException">if propName is null</exception>
+        public static Guid GetGuid(this JObject jObject, string propName)
+        {
+            AssertArgumentNotNull(jObject, "jObject");
+            AssertStringArgumentNotNull(propName, "propName");
+
+            var guidAsString = jObject.Value<string>(propName);
+            return string.IsNullOrEmpty(guidAsString) ? Guid.Empty : Guid.Parse(guidAsString);
         }
 
         /// <summary>
