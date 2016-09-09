19e6cd9a5438fbe9996aed0ea9aedfe1b38b3ecb
Giora Guttsait <giora111@gmail.com>
2016-09-09 22:08:51 +0300

    Refactored Label component
    *	Label is an old component that was never used
    *	Changed to be a GuiComponent

--- XnaTry/XnaClientLib/ECS/Compnents/Label.cs
+++ XnaTry/XnaClientLib/ECS/Compnents/Label.cs
@@ -1,36 +1,58 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
-using XnaCommonLib.ECS.Components;
+using UtilsLib.Consts;
+using UtilsLib.Utility;
 
 namespace XnaClientLib.ECS.Compnents
 {
-    public enum LabelPlacement
+    public class Label : GuiComponent
     {
-        TopLeft,
-        TopCenter,
-        TopRight,
-        MiddleLeft,
-        MiddleCenter,
-        MiddleRight,
-        BottomLeft,
-        BottomCenter,
-        BottomRight
-    }
+        #region Fields
 
-    public class Label : Component
-    {
-        public Label(string text = "", LabelPlacement placement = LabelPlacement.TopCenter, Color? color = null, SpriteFont font = null , bool enabled = true)
-            : base(enabled)
+        private readonly Vector2 position;
+        private readonly string fontAsset;
+        private readonly Func<string> text;
+        private SpriteFont font;
+
+        #endregion Fields
+
+        #region Properties
+
+        public Color Color { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public Label(Func<string> textFunc, Color color, string fontAssetName, Vector2 labelPosition)
         {
-            Text = text;
-            Placement = placement;
-            Font = font;
+            Utils.AssertStringArgumentNotNull(fontAssetName, "fontAssetName");
+
+            text = textFunc;
+            fontAsset = fontAssetName;
             Color = color;
+            position = labelPosition;
         }
 
-        public string Text { get; set; }
-        public LabelPlacement Placement { get; set; }
-        public SpriteFont Font { get; private set; }
-        public Color? Color { get; set; }
+        #endregion
+
+        #region GuiComponent Methods
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.DrawString(font, text(), position, Color);
+        }
+
+        public override void LoadContent(ContentManager content)
+        {
+            font = content.Load<SpriteFont>(fontAsset);
+        }
+
+        public override int DrawOrder => Constants.GUI.DrawOrder.Minimap;
+        public override bool IsHud => true;
+
+        #endregion
     }
 }
