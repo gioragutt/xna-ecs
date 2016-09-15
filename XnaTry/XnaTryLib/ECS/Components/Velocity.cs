83a73d3467386594ce8ff60f626579bc8c87d028
Giora Guttsait <giora111@gmail.com>
2016-09-15 08:57:13 +0300

    Styled Velocity, Added JsonIgnore attributes to helper properties

--- XnaTry/XnaTryLib/ECS/Components/Velocity.cs
+++ XnaTry/XnaTryLib/ECS/Components/Velocity.cs
@@ -1,26 +1,47 @@
 ﻿using Microsoft.Xna.Framework;
+using Newtonsoft.Json;
 
 namespace XnaCommonLib.ECS.Components
 {
     public class Velocity : Component, IUpdatable<Velocity>
     {
+        #region Properties
+
         public Vector2 VelocityVector { get; set; }
+
+        [JsonIgnore]
         public float X => VelocityVector.X;
+
+        [JsonIgnore]
         public float Y => VelocityVector.Y;
+
+        #endregion Properties
+
+        #region Constructor
 
         public Velocity(Vector2 velocityVector)
         {
             VelocityVector = velocityVector;
         }
 
+        #endregion Constructor
+
+        #region IUpdatable<Velocity>
+
         public void Update(Velocity instance)
         {
             VelocityVector = instance.VelocityVector;
         }
 
+        #endregion IUpdatable<Velocity>
+
+        #region Helper Methods
+
         public static Vector2 operator *(Velocity vel, DirectionalInput input)
         {
             return new Vector2(vel.X * input.Horizontal, vel.Y * input.Vertical);
         }
+
+        #endregion
     }
 }
